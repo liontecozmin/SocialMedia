54b8aa56a21bb20bbe5a8f3165316cd005a70967
mariantarciziu <tarciziumarian@yahoo.com>
2017-01-17 13:06:59 +0200

    Update User.cs

--- SC.Core/User.cs
+++ SC.Core/User.cs
@@ -7,42 +7,11 @@ namespace SC.Core
 {
     public class User
     {
-        // [Required]
-        public Guid id { get; set; }
-
-       // [Required]
-       // [MaxLength(50)]
-        public string firstName { get; set; }
-
-        public string lastName { get; set; }
-
-       // [Required]
-        public string email { get; set; }
-
-        public string phoneNumber { get; set; }
-
-        public bool isAdministrator { get; set; }
-
-        public string groups { get; set; }  // grupuri din care face parte
-
-        public string adm_groups { get; set; }  //grupuri asupra carora are drepturi de add,remove user.
-
-        public User(Guid id,string fname, string lname,string email,string phoneNumber,bool isAdministrator,string groups,string adm_groups)
-        {
-            this.id = id;
-            firstName = fname;
-            lastName = lname;
-            this.email = email;
-            this.phoneNumber = phoneNumber;
-            this.isAdministrator = isAdministrator;
-            this.groups = groups;
-            this.adm_groups = adm_groups;
-            
-        }
+        
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Sunt in User.cs");
+            Console.WriteLine("");
             Console.ReadKey();
         }
     }
