1f3709d0bb3f3261a3f7e9f64b3025d44862159b
mariantarciziu <tarciziumarian@yahoo.com>
2017-01-18 20:57:35 +0200

    Add files via upload

---
+++ SC.Core/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SC.Core
+{
+    public class Class
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
